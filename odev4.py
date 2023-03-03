7ecc2df2cd0b4e02b3b584525e0b18c8f5d4a1be
Arzuv Hudaynazarova <126810475+arzuv-hudaynazarova@users.noreply.github.com>
2023-03-03 14:04:35 +0300

    Update odev4.py

--- odev4.py
+++ odev4.py
@@ -1,11 +1,10 @@
 import random
 import time
 
-# Öncelikle, rastgele bir 10000 sayı oluşturacağız:
+# örnek rastgele bir 10000 sayı oluşturacağız:
 arr = [random.randint(1, 1000) for i in range(10000)]
 
-# Daha sonra, birleştirme (merge sort) sıralaması uygulayacağız:
-
+# Merge Sort Algoritması:
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -15,7 +14,6 @@ def merge_sort(arr):
     right = merge_sort(arr[mid:])
 
     return merge(left, right)
-
 
 def merge(left, right):
     result = []
@@ -35,7 +33,24 @@ def merge(left, right):
     return result
 
 
-# BruteForce sırama algoritması
+# Quick Sort Algoritması:
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for i in range(1, len(arr)):
+        if arr[i] < pivot:
+            left.append(arr[i])
+        else:
+            right.append(arr[i])
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# BruteForce Sıralama Algoritması:
 def BruteForceSort(arr):
     n = len(arr)
     for i in range(n):
@@ -44,19 +59,25 @@ def BruteForceSort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-
-
-#Sıralama süresi, aşağıdaki gibi karşılaştırılır:
+# Sıralama süresi, karşılaştırılması işlemi: 
 
 # 1.  Merge Sort algoritması ile sıralama ve çalışma süresinin ölçülmesi
 start_time = time.time()
 sorted_arr = merge_sort(arr)
 end_time = time.time()
-print("MergeSort çalışma süresi: ", end_time - start_time, "saniye")
+print("Merge Sort çalışma süresi: ", end_time - start_time, "saniye")
 
-# 2. BruteForce algoritması ile sıralama ve çalışma süresinin ölçülmesi
+# 2.  Quick Sort algoritması ile sıralama ve çalışma süresinin ölçülmesi
+start_time = time.time()
+sorted_arr1 = quick_sort(arr)
+end_time = time.time()
+print("Quick Sort çalışma süresi: ", end_time - start_time, "saniye")
+
+
+# 1.  BruteForce algoritması ile sıralama ve çalışma süresinin ölçülmesi
 start = time.time()
 BruteForceSort(arr)
 end = time.time()
-print("BruteForce çalışma süresi:", end-start, "saniye")
+print("BruteForce çalışma süresi: ", end - start, "saniye")
+
 
